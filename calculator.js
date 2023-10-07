a1dd740f0793e4767a23b99621b9732d64dc151d
DaniRox <daniroxescobar@gmail.com>
2023-10-07 20:13:26 -0300

    agregando funciones

--- calculator.js
+++ calculator.js
@@ -5,6 +5,48 @@ let monthsResult = document.getElementById("months-result")
 let yearsResult = document.getElementById("years-result")
 
 
+/*let daysMonth = [
+    {
+        "01": 31,
+    },
+    {
+        "02": 28
+    },
+    {
+        "03": 31
+    },
+    {
+        "04": 30
+    },
+    {
+        "05": 31
+    },
+    {
+        "06": 30
+    },
+    {
+        "07": 31
+    },
+    {
+        "08": 31
+    },
+    {
+        "09": 30
+    },
+    {
+        "10": 31
+    },
+    {
+        "11": 30
+    },
+    {
+        "12": 31
+    },
+]*/
+
+let daysMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+
 btnForm.addEventListener("click", function(event){
     event.preventDefault()
 })
@@ -13,16 +55,43 @@ btnForm.addEventListener("click", function(event){
 
 btnForm.addEventListener("click", results);
 
+function totalDays(){
+    let daysTotal;
+    for(let i = 0; i < daysMonth.length ; i++){
+        if(daysMonth[i] > parseInt(month.value)){
+            daysTotal = daysMonth[i] + daysTotal;
+        }
+    }
+    console.log(daysTotal);
+}
 
 function results(){
     console.log(currentMonth);
     let dayImput = parseInt(day.value);
-    let monthImput = parseInt(month.value);
+    let monthImput = parseInt(month.value) - 1;
     let yearImput = parseInt(year.value);
-    daysResult.innerHTML = currentDay;
-    monthsResult.innerHTML = currentMonth - monthImput;
-    yearsResult.innerHTML = currentYear - yearImput;
+    
 
+    if(monthImput > currentMonth){
+        yearsResult.innerHTML = currentYear - yearImput - 1
+    } else {
+        yearsResult.innerHTML = currentYear - yearImput;
+    }
+    let difference;
+    if(monthImput > currentMonth){
+        difference = monthImput - currentMonth;
+        console.log(difference);
+        monthsResult.innerHTML = 12 - difference;
+    } else {
+        monthsResult.innerHTML = currentMonth - monthImput - 1;
+    }
+
+    for(let i = 0; i < daysMonth.length; i ++){
+        daysResult.innerHTML =  currentDay + daysMonth[monthImput] - dayImput;
+    
+    }
+    
 }
+
 
 
--- validacion.js
+++ validacion.js
@@ -1,7 +1,7 @@
 let date = new Date;
 
 let currentYear = date.getFullYear();
-let currentMonth = date.getMonth() + 1; 
+let currentMonth = date.getMonth(); 
 let currentDay = date.getDay() + 1;
 
 let labels = document.querySelectorAll("#label");
@@ -22,84 +22,84 @@ let spanErrorYear = document.querySelector(".span-error-year");
 
 
 
-// function theYear(){
-//     return currentYear;
-// }
+function theYear(){
+    return currentYear;
+}
 
-// day.addEventListener("blur", function (e){
-//     const dayValid = e.currentTarget.value;
-//     if(dayValid > 0 && dayValid < 32) {
-//         valid()
-//     }
-//     if(dayValid.length === 0) {
-//         spanErrorDay.innerHTML = "This field is required";
-//         labelDay.style.color = "hsl(0, 100%, 67%)";
-//         day.style.outline = "1px solid hsl(0, 100%, 67%)";
-//     }
+day.addEventListener("blur", function (e){
+    const dayValid = e.currentTarget.value;
+    if(dayValid > 0 && dayValid < 32) {
+        valid()
+    }
+    if(dayValid.length === 0) {
+        spanErrorDay.innerHTML = "This field is required";
+        labelDay.style.color = "hsl(0, 100%, 67%)";
+        day.style.outline = "1px solid hsl(0, 100%, 67%)";
+    }
 
-//     if(dayValid > 31) {
-//         spanErrorDay.innerHTML = "Must be a valid day";
-//         error();
-//     }
-// })
+    if(dayValid > 31) {
+        spanErrorDay.innerHTML = "Must be a valid day";
+        error();
+    }
+})
 
-// month.addEventListener("blur", function (e){
-//     const monthValid = e.currentTarget.value;
-//     if(monthValid > 0 && monthValid < 13) {
-//         valid()
-//     }
-//     if(monthValid.length === 0) {
-//          spanErrorMonth.innerHTML = "This field is required";
-//          labelMonth.style.color = "hsl(0, 100%, 67%)";
-//          month.style.outline = "1px solid hsl(0, 100%, 67%)";
-//     }
+month.addEventListener("blur", function (e){
+    const monthValid = e.currentTarget.value;
+    if(monthValid > 0 && monthValid < 13) {
+        valid()
+    }
+    if(monthValid.length === 0) {
+         spanErrorMonth.innerHTML = "This field is required";
+         labelMonth.style.color = "hsl(0, 100%, 67%)";
+         month.style.outline = "1px solid hsl(0, 100%, 67%)";
+    }
 
-//     if(monthValid > 12) {
-//         spanErrorMonth.innerHTML = "Must be a valid month";
-//         error();
-//     }
-// })
+    if(monthValid > 12) {
+        spanErrorMonth.innerHTML = "Must be a valid month";
+        error();
+    }
+})
 
-// year.addEventListener("blur", function (e){
-//     const yearValid = e.currentTarget.value;
-//     if(yearValid <= currentYear) {
-//         valid()
-//     }
-//     if(yearValid.length === 0) {
-//          spanErrorYear.innerHTML = "This field is required";
-//          labelYear.style.color = "hsl(0, 100%, 67%)";
-//          year.style.outline = "1px solid hsl(0, 100%, 67%)";
-//     }
+year.addEventListener("blur", function (e){
+    const yearValid = e.currentTarget.value;
+    if(yearValid <= currentYear) {
+        valid()
+    }
+    if(yearValid.length === 0) {
+         spanErrorYear.innerHTML = "This field is required";
+         labelYear.style.color = "hsl(0, 100%, 67%)";
+         year.style.outline = "1px solid hsl(0, 100%, 67%)";
+    }
 
-//     if(yearValid > currentYear) {
-//         spanErrorYear.innerHTML = "Must be a valid year";
-//         error();
-//     }
-// })
+    if(yearValid > currentYear) {
+        spanErrorYear.innerHTML = "Must be a valid year";
+        error();
+    }
+})
 
-// function valid(){
-//     var i;
-//     for (i = 0; i < labels.length; i ++){
-//         labels[i].style.color = "hsl(0, 0%, 8%)";
-//     }
-//     var j;
-//     for (j = 0; j < imputs.length; j ++){
-//         imputs[j].style.outline = "1px solid hsl(0, 0%, 86%)";
-//     }
-//     var h;
-//     for (h = 0; h < spans.length; h ++){
-//         spans[h].innerHTML = "";
-//     }
-// }
+function valid(){
+    var i;
+    for (i = 0; i < labels.length; i ++){
+        labels[i].style.color = "hsl(0, 0%, 8%)";
+    }
+    var j;
+    for (j = 0; j < imputs.length; j ++){
+        imputs[j].style.outline = "1px solid hsl(0, 0%, 86%)";
+    }
+    var h;
+    for (h = 0; h < spans.length; h ++){
+        spans[h].innerHTML = "";
+    }
+}
 
-// function error(){
-//     var i;
-//     for (i = 0; i < labels.length; i ++){
-//         labels[i].style.color = "hsl(0, 100%, 67%)";
-//     }
-//     var j;
-//     for (j = 0; j < imputs.length; j ++){
-//         imputs[j].style.outline = "1px solid hsl(0, 100%, 67%)";
-//     }
+function error(){
+    var i;
+    for (i = 0; i < labels.length; i ++){
+        labels[i].style.color = "hsl(0, 100%, 67%)";
+    }
+    var j;
+    for (j = 0; j < imputs.length; j ++){
+        imputs[j].style.outline = "1px solid hsl(0, 100%, 67%)";
+    }
     
-// }
+}
